21dec7dcfdf2cb69c3cbf101901331e504645056
manasroutcse <manasroutcse@gmail.com>
2023-12-26 19:08:45 +0530

    commit one

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:react/recommended"
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint",
+        "react"
+    ],
+    "rules": {
+        "semi": ["error", "always"],
+        "quotes": ["error", "double"],
+        "import/no-anonymous-default-export": ["error", {
+            "allowArray": false,
+            "allowArrowFunction": false,
+            "allowAnonymousClass": false,
+            "allowAnonymousFunction": false,
+            "allowCallExpression": true, // backward compatibility -> e.g. export default foo(bar)
+            "allowNew": true,
+            "allowLiteral": false,
+            "allowObject": true
+          }]
+    }
+   
+}
